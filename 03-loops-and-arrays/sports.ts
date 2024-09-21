feb0493f0b310102c40aaac1bb15b53051a70860
Graypbj <gbutch8@wgu.edu>
2024-09-21 13:29:35 -0600

    Added sports.ts

---
+++ 03-loops-and-arrays/sports.ts
@@ -0,0 +1,16 @@
+let sportOne: string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
+
+/*
+for (let i=0; i < sportOne.length; i++) {
+    console.log(sportOne[i]);
+}
+    */
+
+// Simplified loop
+
+for (let tempSport of sportOne) {
+    if (tempSport == "Cricket") {
+        console.log(tempSport + " << My Favorite!");
+    }
+    console.log(tempSport);
+}
