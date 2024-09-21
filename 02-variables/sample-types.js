cd732ddd7b4c1198db1d63636c5ea3e1c9cbd30e
Graypbj <gbutch8@wgu.edu>
2024-09-21 13:16:58 -0600

    Added a template string

--- 02-variables/sample-types.js
+++ 02-variables/sample-types.js
@@ -2,6 +2,14 @@ var found = true;
 var grade = 88.6;
 var firstName = "Anup";
 var lastName = 'Kumar';
+// Break it
+/*
+found = 0;
+grade = "A";
+firstName = false;
+*/
 console.log(found);
 console.log("The grade is " + grade);
 console.log("Hi " + firstName + " " + lastName);
+// Use template Strings
+console.log("Hi ".concat(firstName, " ").concat(lastName));
--- 02-variables/sample-types.ts
+++ 02-variables/sample-types.ts
@@ -3,6 +3,17 @@ let grade: number = 88.6;
 let firstName: string = "Anup";
 let lastName: string = 'Kumar';
 
+// Break it
+/*
+found = 0;
+grade = "A";
+firstName = false;
+*/
+
 console.log(found);
 console.log("The grade is " + grade);
+
 console.log("Hi " + firstName + " " + lastName);
+
+// Use template Strings
+console.log(`Hi ${firstName} ${lastName}`);
