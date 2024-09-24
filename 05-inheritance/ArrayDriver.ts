7f4b8183082e2831003c2c2f280983463a8dd67b
Graypbj <gbutch8@wgu.edu>
2024-09-24 10:31:51 -0600

    Updated ArrayDriver.ts

--- 05-inheritance/ArrayDriver.ts
+++ 05-inheritance/ArrayDriver.ts
@@ -3,10 +3,19 @@ import { Circle } from './Circle'
 import { Rectangle } from './Rectangle';
 
 let myShape = new Shape(10, 15);
-console.log(myShape.getInfo());
 
 let myCircle = new Circle(5, 10, 20);
-console.log(myCircle.getInfo());
 
 let myRectangle = new Rectangle(0, 0, 3, 7);
-console.log(myRectangle.getInfo());
+
+// Declare an array of shapes ... initially empty
+let theShapes: Shape[] = [];
+
+// Add the shapes to the array
+theShapes.push(myShape);
+theShapes.push(myCircle);
+theShapes.push(myRectangle);
+
+for (let tempShape of theShapes) {
+    console.log(tempShape.getInfo());
+}
