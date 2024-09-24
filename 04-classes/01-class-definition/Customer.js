e5de6f386b47b9d6b2a5e0fcdb7dea2117a01855
Graypbj <gbutch8@wgu.edu>
2024-09-24 09:43:09 -0600

    Added Customer.ts

---
+++ 04-classes/01-class-definition/Customer.js
@@ -0,0 +1,16 @@
+var Customer = /** @class */ (function () {
+    function Customer(theFirst, theLast) {
+        this.firstName = theFirst;
+        this.lastName = theLast;
+    }
+    return Customer;
+}());
+/*
+let myCustomer = new Customer();
+
+myCustomer.firstName = "Martin";
+myCustomer.lastName = "Dixon";
+*/
+var myCustomer = new Customer("Martin", "Dixon");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
