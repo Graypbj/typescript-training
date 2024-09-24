46d8a54b2867d42c5c20590f4517b20ef83fb4ec
Graypbj <gbutch8@wgu.edu>
2024-09-24 10:00:15 -0600

    Copied and modified Customer.ts

--- 04-classes/03-tsconfig/Customer.js
+++ 04-classes/03-tsconfig/Customer.js
@@ -1,28 +1,37 @@
 "use strict";
-class Customer {
-    constructor(theFirst, theLast) {
+var Customer = /** @class */ (function () {
+    function Customer(theFirst, theLast) {
         this._firstName = theFirst;
         this._lastName = theLast;
     }
-    get firstName() {
-        return this._firstName;
-    }
-    set firstName(value) {
-        this._firstName = value;
-    }
-    get lastName() {
-        return this._lastName;
-    }
-    set lastName(value) {
-        this._lastName = value;
-    }
-}
+    Object.defineProperty(Customer.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (value) {
+            this._lastName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Customer;
+}());
 /*
 let myCustomer = new Customer();
 
 myCustomer.firstName = "Martin";
 myCustomer.lastName = "Dixon";
 */
-let myCustomer = new Customer("Martin", "Dixon");
+var myCustomer = new Customer("Martin", "Dixon");
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastName);
